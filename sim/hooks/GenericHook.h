a0aca22481845549b75d22455941342adbaec811
Peter K <minecraft.mayhem2015@gmail.com>
2025-07-02 22:07:15 -0500

    remove old code

--- sim/hooks/GenericHook.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-// Base class for environment hooks
-class GenericHook {
-public:
-    virtual ~GenericHook() = default;
-    virtual std::string read() { return {}; }
-};
--- sim/hooks/MotorHook.h
+++
@@ -1,5 +0,0 @@
-#include "GenericHook.h"
-class MotorHook : public GenericHook {
-public:
-    std::string read() override { return "motor_env"; }
-};
