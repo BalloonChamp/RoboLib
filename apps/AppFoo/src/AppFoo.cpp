69b748323ca3787ff6cf9f4594323fc141f38f54
Peter K <minecraft.mayhem2015@gmail.com>
2025-07-08 22:55:00 -0500

    Initial Framework
      - Create basic folder structure
      - Create CMakeLists.txt
      - Add .gitignore

---
+++ apps/AppFoo/src/AppFoo.cpp
@@ -0,0 +1,8 @@
+// apps/AppManager/src/AppManager.cpp
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "AppFoo starting up…\n";
+  // TODO: wire up your AppManager logic
+  return 0;
+}
---
+++ apps/AppManager/src/AppManager.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+  std::cout << "AppManager starting up…\n";
+  // TODO: wire up your AppManager logic
+  return 0;
+}
